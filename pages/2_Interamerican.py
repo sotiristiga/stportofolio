9ae66e81ce29741a925398852935f66a02b8b046
sotiristiga <99202968+sotiristiga@users.noreply.github.com>
2025-09-05 14:40:12 +0300

    Update 2_Interamerican.py

--- pages/2_Interamerican.py
+++ pages/2_Interamerican.py
@@ -8,6 +8,27 @@ import plotly.graph_objects as go
 import streamlit as st
 from streamlit_dynamic_filters import DynamicFilters
 
+PASSWORD = "1234"
+
+# Initialize session state
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+
+# Function to handle login
+def login():
+    if st.session_state.password_input == PASSWORD:
+        st.session_state.logged_in = True
+    else:
+        st.error("Incorrect password")
+        st.session_state.logged_in = False
+
+# Only show input if not logged in
+if not st.session_state.logged_in:
+    st.text_input("Enter password", type="password", key="password_input")
+    st.button("Login", on_click=login)
+    if not st.session_state.logged_in:
+        st.stop()
+
 lnk = '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" crossorigin="anonymous">'
 
 
